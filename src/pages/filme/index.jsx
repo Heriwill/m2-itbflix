b75a1ad7d8b27002819e7db49e0b5c8cc41f8ab3
shajardini <sha.jardini@gmail.com>
2024-04-23 20:18:53 -0300

    23/04

---
+++ src/pages/filme/index.jsx
@@ -0,0 +1,9 @@
+import './filme.css'
+
+export default function Filme(){
+    return(
+        <section>
+            <h1>Bem vindo a Filme!</h1>
+        </section>
+    )
+}
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,9 @@
+import './home.css'
+
+export default function Home(){
+    return(
+        <section>
+            <h1>Bem vindo a Home!</h1>
+        </section>
+    )
+}
