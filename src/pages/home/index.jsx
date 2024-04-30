7bff33b3eb40c85f6d01ad7c44b33f6f7ea8530f
shajardini <sha.jardini@gmail.com>
2024-04-30 19:35:24 -0300

    30/04

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,10 @@
+import api from '../../services/api'
+import {useState, useEffect} from 'react'
 import './home.css'
+
+//hooks
+//useState = mudança de estado
+//useEffect = ciclo de vida aplicação
 
 export default function Home(){
     return(
